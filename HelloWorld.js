aec5fca0d38b21b17805afc6feb135b8150aac78
happyjoy2005@gmail.com <happyjoy2005@gmail.com>
2019-01-15 17:18:41 +0900

    init

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,22 @@
-Helloworld
+/*Tag words while Version*/
+var word = '제로초초초'
+while (true){
+var answer = prompt(word)
+if (word[word.length - 1] === answer[0]){
+    word = answer;
+    alert('딩동댕')
+} else {
+    alert('땡')
+    }
+}
+
+/*Tag words For Version*/
+for (var word = '제로초초초'; true; ){
+    var answer = prompt(word)
+    if (word[word.length - 1] === answer[0]){
+        word = answer;
+        alert('딩동댕')
+    } else {
+        alert('땡')
+    }
+}
