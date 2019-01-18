b03b1b1f68e6eed89512010579ef652fb3780cc4
happyjoy2005@gmail.com <happyjoy2005@gmail.com>
2019-01-18 11:50:57 +0900

    eventListener, Handle

--- HelloWorld.js
+++ HelloWorld.js
@@ -26,19 +26,31 @@ var 바디 = document.body;
 var terms = document.createElement('div');
 terms.textContent = '제로초';
 document.body.append(terms);
+
+var form = document.createElement('form');
+document.body.append(form);
+
 var inputWindow = document.createElement('input');
-document.body.append(inputWindow);
+form.append(inputWindow);
+
 var button = document.createElement('button');
 button.textContent = '입력!';
-document.body.append(button);
-var resultWindow = document.createElement('div');
-document.body.append(resultWindow);
+form.append(button);
 
-button.addEventListener('click', function callback () { // fucntion is call backfunction
-    if(terms.textContent[terms.textContent.length - 1] === inputWindow.value [0]){
+var resultWindow = document.createElement('div');
+form.append(resultWindow);
+
+form.addEventListener('submit', function callback (e) { // fucntion is call backfunction
+    e.preventDefault();
+    if(terms.textContent[terms.textContent.length - 1] === inputWindow.value[0]){
         //'안녕하세요'[4]
         resultWindow.textContent = '딩동댕';
+        terms.textContent = inputWindow.value;
+        inputWindow.value = '';
+        inputWindow.focus(); // For User Experience
     }else {
         resultWindow.textContent = '땡!';
+        inputWindow.value = '';
+        inputWindow.focus(); // For User Experience
     }
 });
