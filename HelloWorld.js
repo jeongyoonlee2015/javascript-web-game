aa538b25b8ab71a54475927a293f86931907affb
happyjoy2005@gmail.com <happyjoy2005@gmail.com>
2019-01-18 11:33:57 +0900

    HTML Tag on JS(2)

--- HelloWorld.js
+++ HelloWorld.js
@@ -22,14 +22,23 @@ for (var word = '제로초초초'; true; ){
 }
 */
 //Make tags
-var bodies = document.body;
+var 바디 = document.body;
 var terms = document.createElement('div');
 terms.textContent = '제로초';
 document.body.append(terms);
 var inputWindow = document.createElement('input');
 document.body.append(inputWindow);
 var button = document.createElement('button');
+button.textContent = '입력!';
 document.body.append(button);
-var resultWindow = document.createElement('dive');
+var resultWindow = document.createElement('div');
 document.body.append(resultWindow);
 
+button.addEventListener('click', function callback () { // fucntion is call backfunction
+    if(terms.textContent[terms.textContent.length - 1] === inputWindow.value [0]){
+        //'안녕하세요'[4]
+        resultWindow.textContent = '딩동댕';
+    }else {
+        resultWindow.textContent = '땡!';
+    }
+});
