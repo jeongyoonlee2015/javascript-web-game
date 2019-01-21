1c86c19ee1c628779c77752eb4ec580207f04dc7
happyjoy2005@gmail.com <happyjoy2005@gmail.com>
2019-01-21 21:39:46 +0800

    2nd JS basic with w3schools

---
+++ JavaScript.js
@@ -0,0 +1,33 @@
+/*
+ * JavaScript
+ * with w3schools
+ */
+
+//Array Object
+var cars = ["Iljjeong2", "Gilddong2", "Joy"];
+
+//Boolean Function
+Boolean(7 > 6)
+(7 > 6)
+7 > 6
+
+//Date Object
+var days = new Date();
+var days = new Data(milliseconds);
+
+//Error Reference
+try{
+    alerts("Welcome"); //err = alert
+}catch(err){
+    document.getElementById("demo").innerHTML = err.name + "<br>" + err.message;
+}
+
+//Math Object-Properties
+var x = Math.PI;
+var y = Math.E;
+var z = Math.SQRT2;
+//etc..
+
+//String
+var carName1 = "BMW";
+var carName2 = "MayBach";
